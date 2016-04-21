aa888f9dc5516e46e2cb1c7055e33ae4d69a8593
denis-bogdanas <denis.bogdanas@gmail.com>
2016-04-20 17:39:52 -0700

    Todo items fixed.

--- src/org/oregonstate/droidperm/perm/PermissionDefParser.java
+++ src/org/oregonstate/droidperm/perm/PermissionDefParser.java
@@ -29,7 +29,7 @@ public class PermissionDefParser {
     private static final int INITIAL_SET_SIZE = 10000;
     public static final String CONST_PERM_CHECKER = "_PERM_CHECKER_";
 
-    //todo replace with more specific types, like PermissionDef.
+    //toclean replace with more specific types, like PermissionDef.
     private Set<SootMethodAndClass> permCheckerDefs = new HashSet<>(INITIAL_SET_SIZE);
     private Set<AndroidMethod> sensitiveDefs = new HashSet<>(INITIAL_SET_SIZE);
 
